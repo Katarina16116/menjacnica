cad1620d3fb40da879b7abab22d3bcddd8cf4b27
Katarina16116 <novakovic16116@fon.bg.ac.rs>
2018-03-11 23:38:19 +0100

    Dodata klasa Menjacnica koja implementira IMenjacnica

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,25 @@
+package menjacnica;
+
+import menjacnica_interfejs.IMenjacnica;
+
+public class Menjacnica implements IMenjacnica {
+
+	@Override
+	public void dodajKurs(Kurs k) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKurs(Kurs k) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Kurs pronadjiKurs(Kurs k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
